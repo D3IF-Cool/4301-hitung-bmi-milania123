fd9338c90e8634aad48f759115af9aebb2d73ed9
nandaputrimilania <nandaputrimilania6@gmail.com>
2021-03-16 19:24:56 +0700

    Mengirim data saat bernavigasi

--- app/src/main/java/org/d3ifcool/hitungbmi/ui/SaranFragment.kt
+++ app/src/main/java/org/d3ifcool/hitungbmi/ui/SaranFragment.kt
@@ -6,11 +6,14 @@ import android.view.View
 import android.view.ViewGroup
 import androidx.appcompat.app.AppCompatActivity
 import androidx.fragment.app.Fragment
+import androidx.navigation.fragment.navArgs
 import org.d3ifcool.hitungbmi.R
 import org.d3ifcool.hitungbmi.data.KategoriBmi
 import org.d3ifcool.hitungbmi.databinding.FragmentSaranBinding
 
 class SaranFragment : Fragment() {
+
+    private val args: SaranFragmentArgs by navArgs()
     private lateinit var binding: FragmentSaranBinding
 
     override fun onCreateView(
@@ -20,7 +23,7 @@ class SaranFragment : Fragment() {
     ): View? {
         binding = FragmentSaranBinding.inflate(
             layoutInflater, container, false)
-        updateUI(KategoriBmi.KURUS)
+        updateUI(args.kategori)
         return binding.root
     }
 
