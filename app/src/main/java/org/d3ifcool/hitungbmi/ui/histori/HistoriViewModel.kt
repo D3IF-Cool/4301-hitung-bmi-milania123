8df472a8f6e51e1b72d38c992b504b519d0c581f
nandaputrimilania <nandaputrimilania6@gmail.com>
2021-04-27 20:25:46 +0700

    Menghapus data dari database

--- app/src/main/java/org/d3ifcool/hitungbmi/ui/histori/HistoriViewModel.kt
+++ app/src/main/java/org/d3ifcool/hitungbmi/ui/histori/HistoriViewModel.kt
@@ -2,7 +2,17 @@ package org.d3ifcool.hitungbmi.ui.histori
 
 import BmiDao
 import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.launch
+import kotlinx.coroutines.withContext
 
-class HistoriViewModel(db: BmiDao) : ViewModel() {
+class HistoriViewModel(private val db: BmiDao) : ViewModel() {
     val data = db.getLastBmi()
+
+    fun hapusData() = viewModelScope.launch {
+        withContext(Dispatchers.IO) {
+            db.clearData()
+        }
+    }
 }
