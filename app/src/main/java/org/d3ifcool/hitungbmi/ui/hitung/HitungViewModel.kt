c59c4e836c5a7e4379190f4b48249f27f620d29f
nandaputrimilania <nandaputrimilania6@gmail.com>
2021-04-27 20:15:22 +0700

    Menampilkan data dalam bentuk list

--- app/src/main/java/org/d3ifcool/hitungbmi/ui/hitung/HitungViewModel.kt
+++ app/src/main/java/org/d3ifcool/hitungbmi/ui/hitung/HitungViewModel.kt
@@ -10,6 +10,7 @@ import androidx.lifecycle.viewModelScope
 import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.launch
 import kotlinx.coroutines.withContext
+import org.d3ifcool.hitungbmi.data.HitungBmi
 import org.d3ifcool.hitungbmi.data.KategoriBmi
 
 class HitungViewModel(private val db: BmiDao) : ViewModel() {
@@ -18,31 +19,15 @@ class HitungViewModel(private val db: BmiDao) : ViewModel() {
     private val navigasi = MutableLiveData<KategoriBmi?>()
 
     fun hitungBmi(berat: String, tinggi: String, isMale: Boolean) {
-        val tinggiCm = tinggi.toFloat() / 100
-        val bmi = berat.toFloat() / (tinggiCm * tinggiCm)
-        val kategori = if (isMale) {
-            when {
-                bmi < 20.5 -> KategoriBmi.KURUS
-                bmi >= 27.0 -> KategoriBmi.GEMUK
-                else -> KategoriBmi.IDEAL
-            }
-        }
-        else {
-            when {
-                bmi < 18.5 -> KategoriBmi.KURUS
-                bmi >= 25.0 -> KategoriBmi.GEMUK
-                else -> KategoriBmi.IDEAL
-            }
-        }
-        hasilBmi.value = HasilBmi(bmi, kategori)
+        val dataBmi = BmiEntity(
+            berat = berat.toFloat(),
+            tinggi = tinggi.toFloat(),
+            isMale = isMale
+        )
+        hasilBmi.value = HitungBmi.hitung(dataBmi)
 
         viewModelScope.launch {
             withContext(Dispatchers.IO) {
-                val dataBmi = BmiEntity(
-                    berat = berat.toFloat(),
-                    tinggi = tinggi.toFloat(),
-                    isMale = isMale
-                )
                 db.insert(dataBmi)
             }
         }
